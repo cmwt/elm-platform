2f8e3ecd8fe46df92e86b955065d33066c9addcb
Richard Feldman <richard.t.feldman@gmail.com>
2017-01-04 21:43:26 -0800

    :skull: dead code

--- installers/npm/platform.js
+++ installers/npm/platform.js
@@ -7,8 +7,6 @@ var spawn = require("child_process").spawn;
 var elmVersion = packageInfo.version.replace(/^(\d+\.\d+\.\d+).*$/, "$1");
 var platformDir = path.join(__dirname, "Elm-Platform", elmVersion);
 var distDir = path.join(platformDir, ".cabal-sandbox", "bin");
-var shareDir = path.join(platformDir, "share");
-var shareReactorDir = path.join(shareDir, "reactor");
 var executables = Object.keys(packageInfo.bin);
 var binaryExtension = process.platform === "win32" ? ".exe" : "";
 var executablePaths = {};
@@ -21,8 +19,6 @@ module.exports = {
   packageInfo: packageInfo,
   elmVersion: elmVersion,
   distDir: distDir,
-  shareDir: shareDir,
-  shareReactorDir: shareReactorDir,
   executablePaths: executablePaths,
   executables: executables
 };
